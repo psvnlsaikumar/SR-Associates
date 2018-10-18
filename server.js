c1f046adea0401d3598979de8d70dc9a6c2062ac
sai kumar <saiyah0007@gmail.com>
2018-10-18 17:27:19 +0530

    changed port to be dynamically set

--- server.js
+++ server.js
@@ -59,4 +59,6 @@ app.get('/won', function(req, res){
 });
 
 
-app.listen(8080);
+app.listen((process.env.PORT || 8080,  function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  }));
