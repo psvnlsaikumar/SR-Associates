0d40e67bc0592edfa1a97813774c4c3a4062d898
sai kumar <saiyah0007@gmail.com>
2018-10-26 01:41:35 +0530

    small change

--- server_heroku.js
+++ server_heroku.js
@@ -13,4 +13,4 @@ app.use(express.static("."));
 
 app.listen((process.env.PORT || 8080,  function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  }));.listen(process.env.PORT);
+  })).listen(process.env.PORT);
