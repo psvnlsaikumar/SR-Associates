582fb3f3d8cc48e35b15419bc76dd7fbd9336984
sai kumar <saiyah0007@gmail.com>
2018-10-26 01:53:18 +0530

    server_heroku

--- server_heroku.js
+++ server_heroku.js
@@ -1,9 +1,8 @@
 var http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-app.listen((process.env.PORT || 8080,  function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  })).listen(process.env.PORT);
+const PORT = process.env.PORT
+
+app.listen(PORT);
