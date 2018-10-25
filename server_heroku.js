9f8f1ff55891e3f0a759665032fa0d4d10d46bdf
sai kumar <saiyah0007@gmail.com>
2018-10-26 01:55:08 +0530

    server_heroku

--- server_heroku.js
+++ server_heroku.js
@@ -2,7 +2,13 @@ var http = require('http');
 const express = require('express');
 
 const app = express();
-
+app.set('view engine', 'ejs');
 const PORT = process.env.PORT
+
+
+
+app.get('/', function(req, res){
+    res.render('index');
+});
 
 app.listen(PORT);
