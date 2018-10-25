3ec1425ad5751e01830776e94014a3fffdcf1b57
sai kumar <saiyah0007@gmail.com>
2018-10-26 01:46:20 +0530

    test

--- server_heroku.js
+++ server_heroku.js
@@ -3,13 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-var urlencodedParser = app.use(bodyParser.urlencoded({ extended: false }))
-
-app.set('view engine', 'ejs');
-// app.use('/assets', express.static(__dirname + 'assets'));
-app.use(express.static("."));
-
-
 
 app.listen((process.env.PORT || 8080,  function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
