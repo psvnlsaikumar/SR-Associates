98cdcf5cabbc445eefaa33c86693c7014e46ba02
sai kumar <saiyah0007@gmail.com>
2018-10-26 01:40:32 +0530

    changes made in heroku_server.js to test for heorku running

--- server_heroku.js
+++ server_heroku.js
@@ -1,6 +1,16 @@
 var http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-http.createServer(function (request, response){
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello mailer");
-}).listen(process.env.PORT);
+const app = express();
+var urlencodedParser = app.use(bodyParser.urlencoded({ extended: false }))
+
+app.set('view engine', 'ejs');
+// app.use('/assets', express.static(__dirname + 'assets'));
+app.use(express.static("."));
+
+
+
+app.listen((process.env.PORT || 8080,  function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  }));.listen(process.env.PORT);
